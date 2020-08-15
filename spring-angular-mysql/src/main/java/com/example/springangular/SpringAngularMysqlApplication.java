2c2c37d98011d4c0d529643f6bb42ee591ee599e
krish <krish@DESKTOP-J93R7GQ.hub>
2020-08-16 10:36:25 +1200

    Initial Commit

---
+++ spring-angular-mysql/src/main/java/com/example/springangular/SpringAngularMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularMysqlApplication.class, args);
+	}
+
+}
---
+++ spring-angular-mysql/src/test/java/com/example/springangular/SpringAngularMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
